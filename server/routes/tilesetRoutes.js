fa3061d08f0507e5b381c95111d9f49eafad5b2c
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2022-10-23 23:30:29 -0400

    finished routes for tileset

--- server/routes/tilesetRoutes.js
+++ server/routes/tilesetRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-
-// Tileset Routes
+// Create Tileset
 router.post('/tileset/create', async (req, res) => {
     var tileset = new TileSet({
         tilesetId: new ObjectId(),
-        tiles: [],
+        tileset_data: [],
         name: req.body.name,
         description: "",
         likes: 0,
@@ -20,4 +19,44 @@ router.post('/tileset/create', async (req, res) => {
     res.json({payload: {tileset: tileset}})
 });
 
+// Delete Tileset
+router.delete('/tileset/:id', async (req, res) => {
+    Tileset.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Tileset deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Update tileset
+router.post('/tileset/update/:id', async (req, res) => {
+    Tileset.findById(req.params.id)
+        .then(tileset => {
+            tileset.tileset_data = req.body.tileset_data;
+            tileset.name = req.body.name;
+            tileset.description = req.body.description;
+            tileset.likes = req.body.likes;
+            tileset.dislikes = req.body.dislikes;
+            tileset.comments = req.body.comments;
+            tileset.public = req.body.public;
+            tileset.save()
+                .then(() => res.json('Tileset updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+// Get Tileset
+router.get('/tileset/:id', async (req, res) => {
+    Tileset.findById(req.params.id)
+        .then(tilesets => res.json(tilesets))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+// Get all tilesets
+router.get('/tileset', async (req, res) => {
+    Tileset.find()
+        .then(tilesets => res.json(tilesets))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 module.exports = router;
