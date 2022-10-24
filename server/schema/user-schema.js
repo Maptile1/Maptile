7bb94a7c47d5730301fba8206b3b0bc7bd530f42
Lin Ni <78550234+linni123789@users.noreply.github.com>
2022-10-23 21:59:21 -0400

    User Schema

---
+++ server/schema/user-schema.js
@@ -0,0 +1,61 @@
+const { model, Schema, ObjectId } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        userId: {
+            type: ObjectId,
+            required: true
+        },
+        userName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        tilesets: {
+            type: [ObjectId],
+            required: true
+        },
+        shared_tilesets: {
+            type: [ObjectId],
+            required: true
+        },
+        maps: {
+            type: [ObjectId],
+            required: true
+        },
+        shared_maps: {
+            type: [ObjectId],
+            required: true
+        },
+        likes: {
+            type: Number,
+            required: true
+        },
+        dislikes: {
+            type: Number,
+            required: true
+        },
+        bio: {
+            type: String,
+            required: true
+        },
+        featured: {
+            type: [ObjectId],
+            required: true
+        },
+        accountCreated: {
+            type: Date,
+            required: true
+        }
+    }
+)
+
+const TileSet = model('TileSet', tilesetSchema);
+module.exports = TileSet;
