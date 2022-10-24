3e365b16977222ca854ff8538d8596f36a6f5aaf
RobRosloniec <robrosloniec@gmail.com>
2022-10-23 23:51:55 -0400

    Map Routes Done Maybe?

--- server/routes/mapRoutes.js
+++ server/routes/mapRoutes.js
@@ -25,4 +25,61 @@ router.post('/map/create', async (req, res) => {
     res.json({payload: {tileset: tileset}})
 });
 
+// router.put('/map/update', async (req, res) => {
+//     let {mapId, field, value} = req.body
+
+//     let updated = await Map.findOneAndUpdate({mapId: mapId},{[field]: value})
+//     if(updated) {res.json('Map updated!')}
+// 	else res.status(400).json('Error: ' + err)
+// })
+
+// Get map
+router.get('/map/:id', async (req, res) => {
+    Map.findById(req.params.id)
+        .then(map => res.json(map))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+// Get all maps
+router.get('/map', async (req, res) => {
+    Map.find()
+        .then(maps => res.json(maps))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//Delete Map
+router.delete('/map/:id', async (req, res) => {
+    Map.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Map deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Update Map
+router.post('/map/update/:id', async (req, res) => {
+    Map.findById(req.params.id)
+        .then(map => {
+            map.name = req.body.name;
+            map.tile_width = req.body.tile_width;
+            map.tile_height = req.body.tile_height;
+            map.width = req.body.width;
+            map.height = req.body.height;
+            map.render_order = req.body.render_order;
+            map.description = req.body.description;
+            map.tags = req.body.description;
+            map.likes = req.body.likes;
+            map.dislikes = req.body.dislikes;
+            map.tilesets = req.body.tilesets;
+            map.comments = req.body.comments;
+            map.public = req.body.public;
+            map.mapCreated = req.body.mapCreated;
+            map.layers = req.body.layers;
+            map.owner = req.body.owner; 
+            tileset.save()
+                .then(() => res.json('Tileset updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
