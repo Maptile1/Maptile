133b07989e55ee0e7a848ae898cf93b35b21b08b
Lin Ni <lin.ni@stonybrook.edu>
2022-10-27 09:24:21 -0400

    remove imporitng material ui

---
+++ maptile/src/components/tileset/TilesetScreen.js
@@ -0,0 +1,10 @@
+const TilesetScreen = (props) => {
+  return (
+    <div>
+      <div class="pt-5 text-center text-4xl font-bold text-white">Tileset</div>
+      <div class="box-content ml-bg-white h-32 w-32 border-4 ..."></div>
+    </div>
+  );
+};
+
+export default TilesetScreen;
