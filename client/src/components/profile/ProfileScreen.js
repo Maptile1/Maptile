2e90a7d92bf55f32d343f4ee46ecccaf62f4eb92
Lin Ni <lin.ni@stonybrook.edu>
2022-10-27 12:39:54 -0400

    Profile Screen added Picture name description number of maps/tilesets and likes

--- client/src/components/profile/ProfileScreen.js
+++ client/src/components/profile/ProfileScreen.js
@@ -1,6 +1,27 @@
+import { BsMapFill, BsFillPuzzleFill } from "react-icons/bs";
+import { BiLike } from "react-icons/bi";
 const ProfileScreen = (props) => {
   return (
-    <div class="pt-5 text-center text-4xl font-bold text-white">Profile</div>
+    <div class="grid grid-cols-10 gap-4">
+      <div class="col-start-2 col-span-2 row-start-2 text-white text-center">
+        Joe Schmo
+        <img
+          class="lg:h-48 md:h-36 w-full object-cover object-center"
+          src="https://dummyimage.com/720x400"
+          alt="blog"
+        />
+        Hi my name is Joe Schmo, I like making games!
+      </div>
+      <div class="col-start-5 row-start-2 mt-10 text-6xl justify-self-center text-white">
+        <BsMapFill />8 Maps
+      </div>
+      <div class="col-start-7 row-start-2 mt-10 text-6xl justify-self-center text-white">
+        <BsFillPuzzleFill />8 Tilesets
+      </div>
+      <div class="col-start-9 row-start-2 mt-10 text-6xl justify-self-center text-white">
+        <BiLike />8 Likes
+      </div>
+    </div>
   );
 };
 
--- client/src/components/tileset/TilesetScreen.js
+++ client/src/components/tileset/TilesetScreen.js
@@ -2,9 +2,6 @@ const TilesetScreen = (props) => {
   return (
     <div>
       <div class="pt-5 text-center text-4xl font-bold text-white">Tileset</div>
-      <div class="w-screen h-screen flex justify-center items-center">
-        <div class="box-content bg-white border-4"></div>
-      </div>
     </div>
   );
 };
