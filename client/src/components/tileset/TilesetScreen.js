05f563c936fb6e9d36f89c9910150b64ccdcf12c
Lin Ni <lin.ni@stonybrook.edu>
2022-10-27 11:27:52 -0400

    testing

--- client/src/components/tileset/TilesetScreen.js
+++ client/src/components/tileset/TilesetScreen.js
@@ -1,9 +1,10 @@
-import Box from "@mui/material/Box";
 const TilesetScreen = (props) => {
   return (
     <div>
       <div class="pt-5 text-center text-4xl font-bold text-white">Tileset</div>
-      <div class="box-content ml-100 bg-white h-32 w-32 border-4 ..."></div>
+      <div class="w-screen h-screen flex justify-center items-center">
+        <div class="box-border bg-white h-1820 w-980 border-4"></div>
+      </div>
     </div>
   );
 };
