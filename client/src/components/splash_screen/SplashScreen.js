138ac6be8b6a4f31d18ec4304ea49da5085e65b1
Lin Ni <78550234+linni123789@users.noreply.github.com>
2022-10-28 19:35:46 -0400

    added video

--- client/src/components/splash_screen/SplashScreen.js
+++ client/src/components/splash_screen/SplashScreen.js
@@ -3,9 +3,8 @@ const SplashScreen = (props) => {
 
     return (
         <div class="grid grid-cols-3 grid-rows-5 place-items-center h-screen">
-            <div class="col-start-2 row-start-2 mt-20 ">
-                <img src={"tileset.png"} alt="Logo" />
-            </div>
+            <video class="col-start-2 row-start-2 mt-20 " src={"video.mp4"} autoPlay loop muted>
+            </video>
             <div class="col-start-1 col-span-2 row-start-4">
                 <Button className="h-12 w-60 px-6 m-2 bg-maptile-green shadow-md rounded-lg" ripple={true} onClick={() => props.toggleCreateAccount(true)}>Create Account</Button>
             </div>
