b1f8bd5467e8e004aaeedcb0fa7328dc0e754911
RobRosloniec <robrosloniec@gmail.com>
2022-10-29 15:54:23 -0400

    Added Custom Tailwind Colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
         'maptile-green': '#7FFF00',
         'maptile-green-highlight' : '#55ab00',
         'maptile-purple': '#6A5ACD',
+        'maptile-red': '#FF0000',
+        'maptile-background-dark': '#36393F',
+        'maptile-background-mid': '#575D69',
+        'maptile-background-light': '#7A818E',
+        'maptile-background-bright': '#7A818E',
+        'maptile-tab-unselected': '#454952'
       },
     }
   },
