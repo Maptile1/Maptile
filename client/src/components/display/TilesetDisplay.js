81c91cd2d7f24b0e36eaffabff836b236b2ef447
Lin Ni <78550234+linni123789@users.noreply.github.com>
2022-10-29 19:39:40 -0400

    Some Edits to TilesetDisplay

--- client/src/components/display/TilesetDisplay.js
+++ client/src/components/display/TilesetDisplay.js
@@ -3,17 +3,22 @@ const TilesetDisplay = (props) => {
     return (
         <div>
             <Sidebar />
-            <div class="pt-5 text-center text-4xl font-bold text-white">                    Epic Lava Tileset</div>
-            <div class="grid grid-cols-10">
+            <div class="pt-5 text-center text-4xl font-bold text-white">Epic Lava Tileset</div>
+            <div class="grid grid-cols-8 grid-flow-row">
+                <div class=" col-start-2 mt-10 flex">
+                    <img style={{ width: 100, height: 120, borderRadius: 400 / 2 }}
+                        class=" object-cover"
+                        src="https://www.colorado.edu/today/sites/default/files/styles/medium/public/article-image/liu_s-photo.jpg?itok=l-mJPK65"
+                        alt="blog" />
+                    <div class="text-white ml-2"> @Joe Schmo</div>
+                </div>
 
-                {/* <img
-                    class="w-20 h-20 object-cover object-center"
-                    src="https://www.colorado.edu/today/sites/default/files/styles/medium/public/article-image/liu_s-photo.jpg?itok=l-mJPK65"
-                    alt="blog"
-                /> */}
+
+
             </div>
+            <div class="text-xl text-white">HI</div>
             {/* <div class="col-start-2 ml-28 row-start-3 text-white text-1xl ">  @Joe Schmo</div> */}
-        </div>
+        </div >
 
     );
 };
