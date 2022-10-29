24920edf23b5f91173e86fbe728906d6519adb29
Lin Ni <78550234+linni123789@users.noreply.github.com>
2022-10-29 19:20:35 -0400

    Temp Tileset Display

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import React 			from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homescreen from './components/homescreen/Homescreen';
 import TilesetScreen from './components/tileset/TilesetScreen';
 import MapScreen from './components/map/MapScreen';
 import SearchScreen from './components/search/SearchScreen';
 import ProfileScreen from './components/profile/ProfileScreen';
+import TilesetDisplay from './components/display/TilesetDisplay';
 
 function App() {
 
@@ -15,14 +16,15 @@ function App() {
     <BrowserRouter>
       <div className='app-container'>
         <Routes>
-          <Route exact index path="/" element={<Homescreen user={user}/>}/>
-          <Route path ="/user_tilesets" element={<TilesetScreen user={user}/>}/>
-          <Route path ="/user_maps" element={<MapScreen user={user}/>}/>
-          <Route path ="/search" element={<SearchScreen user={user}/>}/>
-          <Route path ="/user_profile" element={<ProfileScreen user={user}/>}/>
+          <Route exact index path="/" element={<Homescreen user={user} />} />
+          <Route path="/user_tilesets" element={<TilesetScreen user={user} />} />
+          <Route path="/user_maps" element={<MapScreen user={user} />} />
+          <Route path="/search" element={<SearchScreen user={user} />} />
+          <Route path="/user_profile" element={<ProfileScreen user={user} />} />
+          <Route path="/tileset" element={<TilesetDisplay user={user} />} />
         </Routes>
       </div>
-		</BrowserRouter>
+    </BrowserRouter>
   );
 }
 export default App;
--- client/src/components/display/TilesetDisplay.js
+++ client/src/components/display/TilesetDisplay.js
@@ -1,14 +1,20 @@
+import Sidebar from "../sidebar/Sidebar";
 const TilesetDisplay = (props) => {
     return (
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="tileset.png" alt="Sunset in the mountains" />
-            <div class="px-6 py-4">
-                <div class="font-bold text-white text-xl mb-2">{props.tilename}</div>
-                <p class="text-white text-base">
-                    {props.description}
-                </p>
+        <div>
+            <Sidebar />
+            <div class="pt-5 text-center text-4xl font-bold text-white">                    Epic Lava Tileset</div>
+            <div class="grid grid-cols-10">
+
+                {/* <img
+                    class="w-20 h-20 object-cover object-center"
+                    src="https://www.colorado.edu/today/sites/default/files/styles/medium/public/article-image/liu_s-photo.jpg?itok=l-mJPK65"
+                    alt="blog"
+                /> */}
             </div>
+            {/* <div class="col-start-2 ml-28 row-start-3 text-white text-1xl ">  @Joe Schmo</div> */}
         </div>
+
     );
 };
 
