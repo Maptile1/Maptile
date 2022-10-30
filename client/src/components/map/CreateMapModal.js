fc9d9c099fbb2149a58b15da1baa6fa8efdc73f4
RobRosloniec <robrosloniec@gmail.com>
2022-10-29 23:13:22 -0400

    Just a smidge of modal formatting.

--- client/src/components/map/CreateMapModal.js
+++ client/src/components/map/CreateMapModal.js
@@ -2,8 +2,8 @@ import Modal from 'react-modal';
 
 
 const CreateMapModal = (props) => {
-    const buttonInvalid = 'transform rounded-sm py-2 font-bold duration-300 bg-maptile-red-unselected hover:bg-maptile-red rounded-xl w-2/3 mt-10 shadow-lg'
-    const buttonValid = 'transform rounded-sm py-2 font-bold duration-300 bg-maptile-green-highlight hover:bg-maptile-green rounded-xl w-2/3 mt-10 text-white shadow-lg'
+    const buttonInvalid = 'transform rounded-sm py-2 font-bold duration-300 bg-maptile-red-unselected hover:bg-maptile-red rounded-xl w-2/3 mt-10 shadow-lg underline'
+    const buttonValid = 'transform rounded-sm py-2 font-bold duration-300 bg-maptile-green-highlight hover:bg-maptile-green rounded-xl w-2/3 mt-10 text-white shadow-lg underline'
 
     return(
         <Modal isOpen={props.modalOpen} onRequestClose={props.handleClose} contentLabel="Create map"
@@ -12,7 +12,7 @@ const CreateMapModal = (props) => {
           <div className="flex flex-col items-center justify-center">
             <button className="text-white w-full text-right text-lg font-bold opacity-50" onClick={props.handleClose}>X</button>
             <div className="text-white text-4xl underline font-bold">Create Map</div>
-            <div className="text-white mt-8 text-left w-full text-left">Map Name:</div>
+            <div className="text-white mt-8 text-left w-full text-left underline">Map Name:</div>
             <input
               type="text"
               name="name"
@@ -20,7 +20,7 @@ const CreateMapModal = (props) => {
               className="w-full border-none bg-maptile-background-light outline-none placeholder:italic focus:outline-none text-white h-14 p-2.5 rounded-xl"
               onBlur={props.updateInput}
             />
-            <div className="text-white mt-8 w-full text-left">Choose a Tileset:</div>
+            <div className="text-white mt-8 w-full text-left underline">Choose a Tileset:</div>
             <input
               type="text"
               name="tileset"
@@ -29,7 +29,7 @@ const CreateMapModal = (props) => {
               onBlur={props.updateInput}
             />
             <div className="grid grid-cols-5 grid-rows-2 place-items-left w-full">
-              <div className="text-white mt-8 text-left row-start-1">Tile Width:</div>
+              <div className="text-white mt-8 text-left row-start-1 underline">Tile Width:</div>
               <input
               type="text"
               name="tilewidth"
@@ -38,7 +38,7 @@ const CreateMapModal = (props) => {
               onBlur={props.updateInput}
               />
               <div className="text-maptile-green row-start-1 text-center text-5xl font-bold row-span-2 mt-14">X</div>
-              <div className="text-white mt-8 text-left row-start-1 col-start-4">Tile Height:</div>
+              <div className="text-white mt-8 text-left row-start-1 col-start-4 underline">Tile Height:</div>
               <input
               type="text"
               name="tileheight"
@@ -48,7 +48,7 @@ const CreateMapModal = (props) => {
               />
             </div>
             <div className="grid grid-cols-5 grid-rows-2 place-items-left w-full">
-              <div className="text-white mt-8 text-left row-start-1">Map Width:</div>
+              <div className="text-white mt-8 text-left row-start-1 underline">Map Width:</div>
               <input
               type="text"
               name="mapwidth"
@@ -57,7 +57,7 @@ const CreateMapModal = (props) => {
               onBlur={props.updateInput}
               />
               <div className="text-maptile-green row-start-1 text-center text-5xl font-bold row-span-2 mt-14">X</div>
-              <div className="text-white mt-8 text-left row-start-1 col-start-4">Map Height:</div>
+              <div className="text-white mt-8 text-left row-start-1 col-start-4 underline">Map Height:</div>
               <input
               type="text"
               name="mapheight"
--- client/src/components/tileset/CreateTilesetModal.js
+++ client/src/components/tileset/CreateTilesetModal.js
@@ -2,8 +2,8 @@ import Modal from 'react-modal';
 
 const CreateTilesetModal = (props) => {
 
-    const buttonInvalid = 'transform rounded-sm py-2 font-bold duration-300 bg-maptile-red-unselected hover:bg-maptile-red rounded-xl w-2/3 mt-10 shadow-lg'
-    const buttonValid = 'transform rounded-sm py-2 font-bold duration-300 bg-maptile-green-highlight hover:bg-maptile-green rounded-xl w-2/3 mt-10 text-white shadow-lg'
+    const buttonInvalid = 'transform rounded-sm py-2 font-bold duration-300 bg-maptile-red-unselected hover:bg-maptile-red rounded-xl w-2/3 mt-10 shadow-lg underline'
+    const buttonValid = 'transform rounded-sm py-2 font-bold duration-300 bg-maptile-green-highlight hover:bg-maptile-green rounded-xl w-2/3 mt-10 text-white shadow-lg underline'
     return(
         <Modal isOpen={props.modalOpen} onRequestClose={props.handleClose} contentLabel="Create Tileset"
             className="createModal bg-maptile-background-mid w-1/3 h-2/3 rounded-xl"
@@ -11,7 +11,7 @@ const CreateTilesetModal = (props) => {
           <div className="flex flex-col items-center justify-center">
             <button className="text-white w-full text-right text-lg font-bold opacity-50" onClick={props.handleClose}>X</button>
             <div className="text-white text-4xl underline font-bold">Create Tileset</div>
-            <div className="text-white mt-8 text-left w-full text-left">Tileset Name:</div>
+            <div className="text-white mt-8 text-left w-full text-left underline">Tileset Name:</div>
             <input
               type="text"
               name="name"
@@ -19,7 +19,7 @@ const CreateTilesetModal = (props) => {
               className="w-full border-none bg-maptile-background-light outline-none placeholder:italic focus:outline-none text-white h-14 p-2.5 rounded-xl"
               onBlur={props.updateInput}
             />
-            <div className="text-white mt-8 w-full text-left">Upload a Tileset:</div>
+            <div className="text-white mt-8 w-full text-left underline">Upload a Tileset:</div>
             <input
               type="text"
               name="name"
@@ -27,7 +27,7 @@ const CreateTilesetModal = (props) => {
               className="w-full border-none bg-maptile-background-light outline-none placeholder:italic focus:outline-none text-white h-14 p-2.5 rounded-xl"
             />
             <div className="grid grid-cols-5 grid-rows-2 place-items-left w-full">
-              <div className="text-white mt-8 text-left row-start-1">Tile Width:</div>
+              <div className="text-white mt-8 text-left row-start-1 underline">Tile Width:</div>
               <input
               type="text"
               name="tilewidth"
@@ -36,7 +36,7 @@ const CreateTilesetModal = (props) => {
               onBlur={props.updateInput}
               />
               <div className="text-maptile-green row-start-1 text-center text-5xl font-bold row-span-2 mt-14">X</div>
-              <div className="text-white mt-8 text-left row-start-1 col-start-4">Tile Height:</div>
+              <div className="text-white mt-8 text-left row-start-1 col-start-4 underline">Tile Height:</div>
               <input
               type="text"
               name="tileheight"
@@ -46,7 +46,7 @@ const CreateTilesetModal = (props) => {
               />
             </div>
             <div className="grid grid-cols-5 grid-rows-2 place-items-left w-full">
-              <div className="text-white mt-8 text-left row-start-1">Tileset Width:</div>
+              <div className="text-white mt-8 text-left row-start-1 underline">Tileset Width:</div>
               <input
               type="text"
               name="tilesetwidth"
@@ -55,7 +55,7 @@ const CreateTilesetModal = (props) => {
               onBlur={props.updateInput}
               />
               <div className="text-maptile-green row-start-1 text-center text-5xl font-bold row-span-2 mt-14">X</div>
-              <div className="text-white mt-8 text-left row-start-1 col-start-4">Tileset Height:</div>
+              <div className="text-white mt-8 text-left row-start-1 col-start-4 underline">Tileset Height:</div>
               <input
               type="text"
               name="tilesetheight"
