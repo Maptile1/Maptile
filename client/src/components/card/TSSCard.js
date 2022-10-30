595e3c83ca6da7186a6afcfc0f2bcaa2515e216e
Lin Ni <78550234+linni123789@users.noreply.github.com>
2022-10-30 15:21:59 -0400

    fix drop down

--- client/src/components/card/TSSCard.js
+++ client/src/components/card/TSSCard.js
@@ -10,9 +10,9 @@ const TSSCard = (props) => {
         <div class="max-w-sm rounded overflow-hidden mt-5 mx-14">
             <img class="w-full border border-white" src="https://images.gnwcdn.com/2020/usgamer/A-Link-to-the-Past-Map-Header1-05292020.jpg/EG11/thumbnail/1920x1080/format/jpg/quality/65/the-20-best-in-game-maps.jpg" alt="" />
             <div class="grid grid-cols-4">
-                <div class='col-start-1 col-span-2 text-center text-white text-xl underline mt-5'> GREATEST TILESET</div>
+                <div class='col-start-1 col-span-3 text-center text-white text-xl underline mt-5'> GREATEST TILESET</div>
                 <div>
-                    <Menu as="div" className="col-start-2 relative inline-block text-right">
+                    <Menu as="div" className="col-start-3 relative inline-block text-right">
                         <div>
                             <Menu.Button className=" inline-flex w-full justify-center rounded-md bg-opacity-20 px-4 py-2 text-5xl font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
                                 ...
@@ -28,7 +28,7 @@ const TSSCard = (props) => {
                             leaveFrom="transform opacity-100 scale-100"
                             leaveTo="transform opacity-0 scale-95"
                         >
-                            <Menu.Items className=" left-0 mt-2 w-56 origin-top-right  rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <Menu.Items className=" absolute bottom-0 right-0 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                 <div className="px-1 py-1 ">
                                     <Menu.Item>
                                         {({ active }) => (
