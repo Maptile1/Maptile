bd0e3ee8a4ed6cbac5b69f7571abf49c8e31128a
Lin Ni <78550234+linni123789@users.noreply.github.com>
2022-10-30 13:05:18 -0400

    Drop Down Menu for Tileset View

--- client/src/components/display/TilesetDisplay.js
+++ client/src/components/display/TilesetDisplay.js
@@ -1,5 +1,10 @@
 import Sidebar from "../sidebar/Sidebar";
 import { FaThumbsDown, FaThumbsUp } from "react-icons/fa"
+import { Menu, Transition } from '@headlessui/react'
+import { BsSave } from "react-icons/bs"
+import { BiEdit } from "react-icons/bi"
+import { MdOutlineContentCopy } from "react-icons/md"
+import { Fragment, useEffect, useRef, useState } from 'react'
 import Comment from "../comment/Comment";
 const TilesetDisplay = (props) => {
     return (
@@ -21,6 +26,95 @@ const TilesetDisplay = (props) => {
                             </div>
 
                         </div>
+                    </div>
+                    <div class="row-start-1 col-start-7 justify-end mt-5">
+                        <Menu as="div" className="relative inline-block text-left">
+                            <div>
+                                <Menu.Button className="inline-flex w-full justify-center rounded-md bg-opacity-20 px-4 py-2 text-5xl font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                                    ...
+
+                                </Menu.Button>
+                            </div>
+                            <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-100"
+                                enterFrom="transform opacity-0 scale-95"
+                                enterTo="transform opacity-100 scale-100"
+                                leave="transition ease-in duration-75"
+                                leaveFrom="transform opacity-100 scale-100"
+                                leaveTo="transform opacity-0 scale-95"
+                            >
+                                <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right  rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                    <div className="px-1 py-1 ">
+                                        <Menu.Item>
+                                            {({ active }) => (
+                                                <button
+                                                    className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                                                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                >
+                                                    {active ? (
+                                                        <BiEdit
+                                                            className="mr-2 h-5 w-5"
+                                                            aria-hidden="true"
+                                                        />
+                                                    ) : (
+                                                        <BiEdit
+                                                            className="mr-2 h-5 w-5"
+                                                            aria-hidden="true"
+                                                        />
+                                                    )}
+                                                    Edit
+                                                </button>
+                                            )}
+                                        </Menu.Item>
+                                        <Menu.Item>
+                                            {({ active }) => (
+                                                <button
+                                                    className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                                                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                >
+                                                    {active ? (
+                                                        <MdOutlineContentCopy
+                                                            className="mr-2 h-5 w-5"
+                                                            aria-hidden="true"
+                                                        />
+                                                    ) : (
+                                                        <MdOutlineContentCopy
+                                                            className="mr-2 h-5 w-5"
+                                                            aria-hidden="true"
+                                                        />
+                                                    )}
+                                                    Save to Shared
+                                                </button>
+                                            )}
+                                        </Menu.Item>
+
+                                        <Menu.Item>
+                                            {({ active }) => (
+                                                <button
+                                                    className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                                                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                >
+                                                    {active ? (
+                                                        <BsSave
+                                                            className="mr-2 h-5 w-5"
+                                                            aria-hidden="true"
+                                                        />
+                                                    ) : (
+                                                        <BsSave
+                                                            className="mr-2 h-5 w-5"
+                                                            aria-hidden="true"
+                                                        />
+                                                    )}
+                                                    Download
+                                                </button>
+                                            )}
+                                        </Menu.Item>
+                                    </div>
+
+                                </Menu.Items>
+                            </Transition>
+                        </Menu>
                     </div>
                     <div class="mt-10 row-start-2 col-span-3">Tags: Destiny, Space, Adventure, Gray</div>
                     <div class="row-start-3 row-end-4 col-start-1 col-end-4 bg-white mb-10">
