91e81a820794afbb43cc7c81db3a237eb527c8e4
Michael Chan <mchan5205@gmail.com>
2022-10-31 12:11:15 -0400

    added bcrypt to hash password in database

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,37 +2,47 @@ const express = require('express');
 const User = require('../schema/user-schema');
 const userRouter = express.Router();
 const ObjectId = require('mongodb').ObjectId;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 userRouter.route('/user/register').post(async (req, res) => {
-    var user = new User({
-        _id: new ObjectId(),
-        userName: req.body.userName,
-        email: req.body.email,
-        password: req.body.password, //hash later...
-        tilesets: [],
-        shared_tilesets: [],
-        maps: [],
-        shared_maps: [],
-        likes: 0,
-        dislikes: 0,
-        bio: "bio",
-        featured: [],
-        accountCreated: Date.now()
+    bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+        var user = new User({
+            _id: new ObjectId(),
+            userName: req.body.userName,
+            email: req.body.email,
+            password: hash,
+            tilesets: [],
+            shared_tilesets: [],
+            maps: [],
+            shared_maps: [],
+            likes: 0,
+            dislikes: 0,
+            bio: "bio",
+            featured: [],
+            accountCreated: Date.now()
+        })
+        await user.save()
+            .then((user) => {res.json({user: user})})
+            .catch((err) => {res.json({errorMessage: err.message})})
     })
-    await user.save()
-        .then((user) => {res.json({user: user})})
-        .catch((err) => {res.json({errorMessage: err.message})})
 })
 
 userRouter.route('/user/login').post(async (req, res) => {
-    //hash password later bcrypt...
-    var user = await User.findOne({userName: req.body.userName, password: req.body.password})
+    var user = await User.findOne({userName: req.body.userName})
     if (user == null){
         res.status(400).json({errorMessage: "Username password combination not found"})
     }
     else{
-        req.session._id = user._id.toString()
-        res.json({user: user})
+        bcrypt.compare(req.body.password, user.password, (err, result) => {
+            if (result){
+                req.session._id = user._id.toString()
+                res.json({user: user})
+            }
+            else{
+                res.status(400).json({errorMessage: "Username password combination not found"})
+            }
+        })
     }
 })
 
