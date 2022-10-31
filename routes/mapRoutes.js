111054db1fe51ba40b375333cfc985b1f65a71ad
Michael Chan <mchan5205@gmail.com>
2022-10-31 11:26:40 -0400

    changed map update to be more secure and error check

--- routes/mapRoutes.js
+++ routes/mapRoutes.js
@@ -67,27 +67,32 @@ router.post('/map/delete/:id', async (req, res) => {
 
 // Update Map
 router.post('/map/update/:id', async (req, res) => {
-    Map.findById(req.params.id)
-        .then(map => {
-            map.name = req.body.name || map.name;
-            map.tile_width = req.body.tile_width || map.tile_width;
-            map.tile_height = req.body.tile_height || map.tile_height;
-            map.width = req.body.width || map.width;
-            map.height = req.body.height || map.height;
-            map.render_order = req.body.render_order || map.render_order;
-            map.description = req.body.description || map.description;
-            map.tags = req.body.tags || map.tags;
-            map.likes = req.body.likes || map.likes;
-            map.dislikes = req.body.dislikes || map.dislikes;
-            map.tilesets = req.body.tilesets || map.tilesets;
-            map.comments = req.body.comments || map.comments;
-            map.public = req.body.public || map.public;
-            map.layers = req.body.layers || map.layers;
-            map.save()
-                .then(() => res.json('Map updated!'))
-                .catch(err => res.status(400).json('Error: ' + err));
-        })
-        .catch(err => res.status(400).json('Error: ' + err));
+    if (req.session._id == undefined){
+        res.status(400).json({errorMessage: 'Not logged in'})
+        return;
+    }
+    var updates = {}
+    updates.name = req.body.name
+    updates.tile_width = req.body.tile_width
+    updates.width = req.body.width
+    updates.height = req.body.height
+    updates.render_order = req.body.render_order
+    updates.description = req.body.description
+    updates.tilesets = req.body.tilesets
+    updates.public = req.body.public
+    updates.layers = req.body.layers
+    var map = await Map.findOneAndUpdate({_id: req.params.id, owner: req.session._id}, {$set: updates}, {new: true})
+    if (map != null){
+        res.json({map: map})
+    }
+    else{
+        if (await Map.findById(req.params.id) != null){
+            res.status(400).json({errorMessage: 'Not permitted to editle'})
+        }
+        else{
+            res.status(400).json({errorMessage: 'Tileset does not exist'})
+        }
+    }
 });
 
 module.exports = router;
