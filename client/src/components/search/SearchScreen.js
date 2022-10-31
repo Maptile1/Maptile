5db4c62b6641af0aae35402291a25eed0f67d25b
RobRosloniec <robrosloniec@gmail.com>
2022-10-31 16:22:19 -0400

    Touched Up Search Screen

--- client/src/components/search/SearchScreen.js
+++ client/src/components/search/SearchScreen.js
@@ -25,14 +25,14 @@ const SearchScreen = (props) => {
         <div className="pt-5 text-center text-4xl font-bold text-white underline">Search</div>
         <div className="flex flex-col h-[53rem] w-5/6 items-left justify-top ml-20 mt-10">
           <div class="grid grid-cols-4">
-            <form class="mb-10 col-start-4 col-span-3">
+            <form className="mb-10 col-start-4 col-span-3">
               <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
-              <div class="relative w-100">
-                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                  <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+              <div className="relative w-100">
+                <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                  <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
-                <input type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-white-500 focus:border-black-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Tilesets and Maps" required />
-                <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-maptile-background-mid hover:bg-black focus:ring-4 focus:outline-none focus:ring-white-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                <input type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-maptile-background-mid rounded-lg border border-gray-300 focus:ring-white-500 focus:border-black-500 dark:bg-maptile-background-mid dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Tilesets and Maps" required />
+                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-maptile-background-mid hover:bg-black focus:ring-4 focus:outline-none focus:ring-white-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-maptile-green dark:hover:bg-maptile-green-highlight dark:focus:ring-blue-800">Search</button>
               </div>
             </form>
           </div>
@@ -47,14 +47,14 @@ const SearchScreen = (props) => {
                 <TSSCard search={true} name={obj.name} />
               )}
             </div>
-            <div class="absolute text-white top-10 mt-48 mr-10 right-4">
-              <div class="text-3xl font-bold text-center"> Tags</div>
-              <div class="bg-maptile-background-mid w-full mt-5 h-[30rem] overflow-auto ">
-                <input class=" bg-maptile-background-light mt-5 w-3/4 flex ml-7"></input>
-                <div class="flex flex-col ml-7 mt-5 space-y-4">
+            <div className="absolute text-white top-10 mt-48 mr-10 right-4">
+              <div className="text-3xl font-bold text-center"> Tags</div>
+              <div className="bg-maptile-background-mid w-full mt-5 h-[30rem] overflow-auto rounded-xl ">
+                <input className=" bg-maptile-background-light mt-5 w-3/4 flex ml-7 rounded-xl p-2"></input>
+                <div className="flex flex-col ml-7 mt-5 space-y-4">
                   {tags.map((obj, index) =>
                     <div>
-                      <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-green-100 bg-black rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                      <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-green-100 bg-black rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                       <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{obj}</label>
                     </div>
                   )}
