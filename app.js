1b5f1f4322e5c17d5a28f0937f1ebd37353a025f
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2022-11-06 14:44:42 -0500

    changed package.json to deploy both frontend and backend on heroku:

--- app.js
+++ app.js
@@ -42,6 +42,16 @@ app.use("/", mapRoutes);
 const commentRoutes = require("./routes/commentRoutes");
 app.use("/", commentRoutes);
 
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 // port
 const port = process.env.PORT || 8080;
 
