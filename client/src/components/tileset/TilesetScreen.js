2c95665f98dd3a427eaa39e6f75b6d8168123974
Lin Ni <lin.ni@stonybrook.edu>
2022-11-08 12:03:50 -0500

    Changed to create tileset not requiring description

--- client/src/components/tileset/TilesetScreen.js
+++ client/src/components/tileset/TilesetScreen.js
@@ -3,68 +3,131 @@ import React, { useState } from "react";
 import CreateTilesetModal from "./CreateTilesetModal";
 import TSSCard from "../card/TSSCard";
 import ShareModal from "../map/ShareModal";
+import Axios from "axios";
 const TilesetScreen = (props) => {
-  const [userSelected, updateUserSelected] = useState(true)
-  const [modalOpen, setModal] = useState(false)
-  const [input, setInput] = useState({ name: '', tilewidth: '', tileheight: '', tilesetwidth: '', tilesetheight: '' });
-  const [inputValid, setInputValid] = useState(false)
-  const [shareModalOpen, setShareModal] = useState(false)
-  let tab_selected = 'bg-maptile-background-mid text-center rounded-t-xl cursor-pointer  mt-[10px] duration-300'
-  let tab_unselected = 'bg-maptile-tab-unselected text-center rounded-t-xl cursor-pointer duration-300'
+  const [userSelected, updateUserSelected] = useState(true);
+  const [modalOpen, setModal] = useState(false);
+  const [input, setInput] = useState({
+    name: "",
+    tilewidth: "",
+    tileheight: "",
+    tilesetwidth: "",
+    tilesetheight: "",
+  });
+  const [inputValid, setInputValid] = useState(false);
+  const [shareModalOpen, setShareModal] = useState(false);
+  let tab_selected =
+    "bg-maptile-background-mid text-center rounded-t-xl cursor-pointer  mt-[10px] duration-300";
+  let tab_unselected =
+    "bg-maptile-tab-unselected text-center rounded-t-xl cursor-pointer duration-300";
 
   const updateInput = (e) => {
     const { name, value } = e.target;
     const updated = { ...input, [name]: value };
     setInput(updated);
-    setInputValid(updated.name !== '' && updated.tilewidth !== '' && updated.tileheight !== '' && updated.tilesetwidth !== '' && updated.tilesetheight !== '');
+    setInputValid(
+      updated.name !== "" &&
+        updated.tilewidth !== "" &&
+        updated.tileheight !== "" &&
+        updated.tilesetwidth !== "" &&
+        updated.tilesetheight !== ""
+    );
   };
 
-  const handleCreate = (e) => {
+  const handleCreate = async (e) => {
     if (inputValid) {
-      setModal(false)
+      setModal(false);
+      var response = await Axios.post(
+        "https://maptile1.herokuapp.com/tileset/create",
+        {
+          name: input.name,
+          _id: props.user._id,
+        }
+      );
+      console.log(response);
     }
-  }
+  };
 
   const handleClose = (e) => {
-    setInput({ name: '', tilewidth: '', tileheight: '', tilesetwidth: '', tilesetheight: '' })
-    setInputValid(false)
-    setModal(false)
-  }
+    setInput({
+      name: "",
+      tilewidth: "",
+      tileheight: "",
+      tilesetwidth: "",
+      tilesetheight: "",
+    });
+    setInputValid(false);
+    setModal(false);
+  };
 
-  const tilesets = []
+  const tilesets = [];
   let tileset1 = { name: "Great Tileset" };
   let tileset2 = { name: "Best Tileset" };
-  let tileset3 = { name: "Nice Tileset" }
-  let tileset4 = { name: "Water Tileset" }
-  let tileset5 = { name: "Grass Tileset" }
-  let tileset6 = { name: "Lava Tileset" }
-  let tileset7 = { name: "Small Tileset" }
-  let tileset8 = { name: "Large Tileset" }
+  let tileset3 = { name: "Nice Tileset" };
+  let tileset4 = { name: "Water Tileset" };
+  let tileset5 = { name: "Grass Tileset" };
+  let tileset6 = { name: "Lava Tileset" };
+  let tileset7 = { name: "Small Tileset" };
+  let tileset8 = { name: "Large Tileset" };
 
-  tilesets.push(tileset1, tileset2, tileset3, tileset4, tileset5, tileset6, tileset7, tileset8)
+  tilesets.push(
+    tileset1,
+    tileset2,
+    tileset3,
+    tileset4,
+    tileset5,
+    tileset6,
+    tileset7,
+    tileset8
+  );
   return (
     <div>
       <Sidebar />
 
       <main className="mx-auto flex flex-col min-h-screen w-full items-center justify-top bg-maptile-background-dark text-white">
-        <div className="pt-5 text-center text-4xl font-bold text-white">Tileset</div>
+        <div className="pt-5 text-center text-4xl font-bold text-white">
+          Tileset
+        </div>
         <div className="flex flex-col h-[53rem] w-5/6 items-left justify-top ml-20 mt-10">
           <div className="grid grid-cols-8 grid-rows-1 place-items-left w-full">
-            <div className={`${userSelected ? tab_selected : tab_unselected}`} onClick={() => updateUserSelected(true)}>User Tilesets</div>
-            <div className={`${!userSelected ? tab_selected : tab_unselected}`} onClick={() => updateUserSelected(false)}>Shared Tilesets</div>
-            <div className="text-maptile-green col-start-8 text-right text-4xl cursor-pointer" onClick={() => setModal(true)}>+</div>
+            <div
+              className={`${userSelected ? tab_selected : tab_unselected}`}
+              onClick={() => updateUserSelected(true)}
+            >
+              User Tilesets
+            </div>
+            <div
+              className={`${!userSelected ? tab_selected : tab_unselected}`}
+              onClick={() => updateUserSelected(false)}
+            >
+              Shared Tilesets
+            </div>
+            <div
+              className="text-maptile-green col-start-8 text-right text-4xl cursor-pointer"
+              onClick={() => setModal(true)}
+            >
+              +
+            </div>
           </div>
           <div className="bg-maptile-background-mid w-full h-[50rem] rounded-r-xl rounded-b-xl overflow-auto">
             <div className="flex flex-row flex-wrap px-5 py-5 pl-10  ">
-              {tilesets.map((obj, index) =>
+              {tilesets.map((obj, index) => (
                 <TSSCard name={obj.name} />
-              )}
+              ))}
             </div>
-            <CreateTilesetModal modalOpen={modalOpen} inputValid={inputValid} updateInput={updateInput} handleCreate={handleCreate} handleClose={handleClose} />
-            <ShareModal modalOpen={shareModalOpen} setShareModal={setShareModal} />
+            <CreateTilesetModal
+              modalOpen={modalOpen}
+              inputValid={inputValid}
+              updateInput={updateInput}
+              handleCreate={handleCreate}
+              handleClose={handleClose}
+            />
+            <ShareModal
+              modalOpen={shareModalOpen}
+              setShareModal={setShareModal}
+            />
           </div>
         </div>
-
       </main>
     </div>
   );
--- routes/tilesetRoutes.js
+++ routes/tilesetRoutes.js
@@ -1,103 +1,101 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Tileset = require('../schema/tileset-schema');
-const ObjectId = require('mongodb').ObjectId;
+const Tileset = require("../schema/tileset-schema");
+const ObjectId = require("mongodb").ObjectId;
 const User = require("../schema/user-schema");
 
 // Create Tileset
-router.post('/tileset/create', async (req, res) => {
-    // if (req.session._id == undefined){
-    //     res.status(400).json({errorMessage: 'Not logged in'})
-    //     return;
-    // }
-    var tilesetid = new ObjectId()
-    var tileset = new Tileset({
-        _id: tilesetid,
-        tileset_data: [],
-        name: req.body.name,
-        description: req.body.description,
-        likes: 0,
-        dislikes: 0,
-        comments: [],
-        public: false,
-        tilesetCreated: Date.now(),
-        owner: req.body._id
-    })
-    await tileset.save()
-    // res.json({ tileset: tileset })
+router.post("/tileset/create", async (req, res) => {
+  // if (req.session._id == undefined){
+  //     res.status(400).json({errorMessage: 'Not logged in'})
+  //     return;
+  // }
+  var tilesetid = new ObjectId();
+  var tileset = new Tileset({
+    _id: tilesetid,
+    tileset_data: [],
+    name: req.body.name,
+    description: "",
+    likes: 0,
+    dislikes: 0,
+    comments: [],
+    public: false,
+    tilesetCreated: Date.now(),
+    owner: req.body._id,
+  });
+  await tileset.save();
+  // res.json({ tileset: tileset })
 
-    // var user = await User.findById(req.body._id)
-    // var newtilesetarray = user.tilesets.push(tilesetid)
-    // var newuser = await User.updateOne(
-    //     { _id: req.body._id }, //temp
-    //     { tilesets: newtilesetarray },
-    // )
-})
+  // var user = await User.findById(req.body._id)
+  // var newtilesetarray = user.tilesets.push(tilesetid)
+  // var newuser = await User.updateOne(
+  //     { _id: req.body._id }, //temp
+  //     { tilesets: newtilesetarray },
+  // )
+});
 
 // Delete Tileset
-router.post('/tileset/delete/:id', async (req, res) => {
-    if (req.session._id == undefined) {
-        res.status(400).json({ errorMessage: 'Not logged in' })
-        return;
-    }
-    Tileset.findOneAndRemove({ _id: req.params.id, owner: req.session._id })
-        .then(() => res.json({ message: 'Tileset deleted' }))
-        .catch(err => {
-            Tileset.findOne({ _id: req.params.id })
-                .then((tileset) => {
-                    if (tileset != null) {
-                        res.status(400).json({ errorMessage: 'Not owner of tileset' })
-                    }
-                    else {
-                        res.status(400).json({ errorMessage: 'Tileset does not exist' })
-                    }
-                })
-                .catch(err => res.status(400).json({ errorMessage: err }))
+router.post("/tileset/delete/:id", async (req, res) => {
+  if (req.session._id == undefined) {
+    res.status(400).json({ errorMessage: "Not logged in" });
+    return;
+  }
+  Tileset.findOneAndRemove({ _id: req.params.id, owner: req.session._id })
+    .then(() => res.json({ message: "Tileset deleted" }))
+    .catch((err) => {
+      Tileset.findOne({ _id: req.params.id })
+        .then((tileset) => {
+          if (tileset != null) {
+            res.status(400).json({ errorMessage: "Not owner of tileset" });
+          } else {
+            res.status(400).json({ errorMessage: "Tileset does not exist" });
+          }
         })
-})
+        .catch((err) => res.status(400).json({ errorMessage: err }));
+    });
+});
 
 // Update tileset
-router.post('/tileset/update/:id', async (req, res) => {
-    if (req.session._id == undefined) {
-        res.status(400).json({ errorMessage: 'Not logged in' })
-        return;
+router.post("/tileset/update/:id", async (req, res) => {
+  if (req.session._id == undefined) {
+    res.status(400).json({ errorMessage: "Not logged in" });
+    return;
+  }
+  var updates = {};
+  updates.tileset_data = req.body.tileset_data;
+  updates.name = req.body.name;
+  updates.description = req.body.description;
+  updates.public = req.body.public;
+  var tileset = await Tileset.findOneAndUpdate(
+    { _id: req.params.id, owner: req.session._id },
+    { $set: updates },
+    { new: true }
+  );
+  if (tileset != null) {
+    res.json({ tileset: tileset });
+  } else {
+    if ((await Tileset.findById(req.params.id)) != null) {
+      res.status(400).json({ errorMessage: "Not permitted to editle" });
+    } else {
+      res.status(400).json({ errorMessage: "Tileset does not exist" });
     }
-    var updates = {};
-    updates.tileset_data = req.body.tileset_data
-    updates.name = req.body.name
-    updates.description = req.body.description
-    updates.public = req.body.public
-    var tileset = await Tileset.findOneAndUpdate({ _id: req.params.id, owner: req.session._id }, { $set: updates }, { new: true })
-    if (tileset != null) {
-        res.json({ tileset: tileset })
-    }
-    else {
-        if (await Tileset.findById(req.params.id) != null) {
-            res.status(400).json({ errorMessage: 'Not permitted to editle' })
-        }
-        else {
-            res.status(400).json({ errorMessage: 'Tileset does not exist' })
-        }
-    }
+  }
 });
-
 
 // Get Tileset
-router.get('/tileset/get/:id', async (req, res) => {
-    var tileset = await Tileset.findById(req.params.id)
-    if (tileset != null) {
-        res.json({ tileset: tileset })
-    }
-    else {
-        res.status(400).json({ errorMessage: 'Tileset does not exist' })
-    }
+router.get("/tileset/get/:id", async (req, res) => {
+  var tileset = await Tileset.findById(req.params.id);
+  if (tileset != null) {
+    res.json({ tileset: tileset });
+  } else {
+    res.status(400).json({ errorMessage: "Tileset does not exist" });
+  }
 });
 
-
 // Get all tilesets
-router.get('/tileset', async (req, res) => {
-    Tileset.find()
-        .then(tilesets => res.json(tilesets))
-        .catch(err => res.status(400).json('Error: ' + err));
+router.get("/tileset", async (req, res) => {
+  Tileset.find()
+    .then((tilesets) => res.json(tilesets))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 module.exports = router;
