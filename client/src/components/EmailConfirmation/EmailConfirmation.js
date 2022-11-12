b03af7b4b45480847ea3e3a52d00d171a0b1233b
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2022-11-11 23:39:29 -0500

    added code confirmation

--- client/src/components/EmailConfirmation/EmailConfirmation.js
+++ client/src/components/EmailConfirmation/EmailConfirmation.js
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
+import Axios from 'axios';
 
 const EmailConfirmation = (props) => {
 
     const [inputValid, setInputValid] = useState(false)
     const [code, setCode] = useState("");
+    const [email, setEmail] = useState("");
 
     const closeView = () => {
         props.toggleEmailConfirmationView(false);
         props.toggleForgotPasswordView(true);
     }
 
-    const handleChange = (event) => {
+    const validEmail = (email) => {
+      return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+    }
+
+    const handleChangeEmail = (event) => {
+      if(event.target.value.length !== 0 && validEmail(event.target.value)){
+        setInputValid(true);
+      }
+      setEmail(event.target.value);
+    }
+
+    const handleChangeCode = (event) => {
         if(event.target.value.length !== 0){
             setInputValid(true);
         }
@@ -18,24 +35,51 @@ const EmailConfirmation = (props) => {
     }
 
     const confirmResetPasswordCode = () => {
-        console.log(code)
-        if(Number(code) === 12345){
-            // continue to reset password
-            props.toggleEmailConfirmationView(false);
-            props.toggleResetPasswordView(true);
+      Axios.get("http://localhost:8080/user/email/" + email)
+      .then(
+        function(response){
+          let user = response.data.user[0];
+          if(user !== undefined){
+            if(Number(code) === user.recoveryCode){
+              // continue to reset password
+              props.toggleEmailConfirmationView(false);
+              props.toggleResetPasswordView(true);
+          }
+          else{
+              // deny access
+              window.alert("Invalid code.")
+              // maybe reset code and kick user out
+          }
+          }
+          else{
+            window.alert("User with email " + email + " not found.")
+          }
         }
-        else{
-            // deny access
-        }
+      )
+      .catch(function(error){
+        window.alert(error.response.data.errorMessage)
+      });
     }
 
     return (
         <main
         class="mx-auto flex min-h-screen w-full items-center justify-center bg-maptile-background-dark text-white"
       >
-        <div class="bg-maptile-purple flex w-[40rem] h-[25rem] justify-center align-middle shadow-md rounded-lg">
+        <div class="bg-maptile-purple flex w-[40rem] h-[28rem] justify-center align-middle shadow-md rounded-lg">
           <section class="flex w-[30rem] flex-col space-y-10 mt-10">
             <div class="text-center text-4xl font-medium">Enter the five-digit code we sent to your email</div>
+
+            <div
+              class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500"
+            >
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter email"
+                class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+                onChange={handleChangeEmail}
+              />
+            </div>
 
             <div
               class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500"
@@ -45,7 +89,8 @@ const EmailConfirmation = (props) => {
                 name="number"
                 placeholder="Enter email confirmation code"
                 class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
-                onChange={handleChange}
+                maxlength="5"
+                onChange={handleChangeCode}
               />
             </div>
             <button 
