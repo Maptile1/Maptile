da640b4b8b8acf46db135266de89af9150203bf9
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2022-11-16 23:47:31 -0500

    fixed axios get req

--- client/src/components/display/TilesetDisplay.js
+++ client/src/components/display/TilesetDisplay.js
@@ -75,7 +75,7 @@ const TilesetDisplay = (props) => {
 
     const getComments = async () => {
         setLoading(true);
-        await Axios.get("https://maptile1.herokuapp.com/comment/:id")
+        await Axios.get("https://maptile1.herokuapp.com/comment/" + id)
         .then((response) => {
             setComments(response.data.comments);
         })
@@ -85,7 +85,7 @@ const TilesetDisplay = (props) => {
         setLoading(false);
     }
     getComments();
-  }, [location.state._id, location.state.owner, id]);
+  }, [location.state._id, location.state.owner, id, comments]);
 
   return (
     <div>
