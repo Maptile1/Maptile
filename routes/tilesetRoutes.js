e2dab196da9d850ed5c5945898d9ca4fec674f3e
Michael Chan <mchan5205@gmail.com>
2022-11-20 17:53:42 -0500

    added a route for getting multiple tilesets

--- routes/tilesetRoutes.js
+++ routes/tilesetRoutes.js
@@ -312,4 +312,12 @@ router.post("/tileset/dislike/:id", async (req, res) => {
   }
 })
 
+router.post("/tileset/getBatch", async (req, res) => {
+  var limit = req.body.limit ? req.body.limit : 0
+  var page = req.body.page ? req.body.page * limit : 0
+  var ids = req.body.ids.map((id) => { return {_id: id}})
+  var tilesets = await Tileset.find({$or: ids}).sort({timeEdited : -1, _id: 1}).skip(page).limit(limit)
+  res.json({tilesets: tilesets})
+})
+
 module.exports = router;
