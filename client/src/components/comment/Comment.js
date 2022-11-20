2eb48fe3dcd4e080e70939d7147520f8fb98d8a6
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2022-11-20 18:34:40 -0500

    fixed comment formatting

--- client/src/components/comment/Comment.js
+++ client/src/components/comment/Comment.js
@@ -1,10 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
 const Comment = (props) => {
+
+    const [owner, setOwner] = useState(null);
+    const [userPfp, setPfp] = useState("");
+
+    useEffect(() => {
+        const getOwner = async () => {
+            await Axios.get(
+                "https://maptile1.herokuapp.com/user/get/" + props.owner
+              ).then((response) => {
+                console.log(response.data.user);
+                setOwner(response.data.user);
+                setPfp(
+                  "https://maptilefiles.blob.core.windows.net/maptile-profile-images/" +
+                  response.data.user._id
+                );
+              });
+        }
+        getOwner();
+    }, []);
+
     return (<div class="relative gap-4 p-4 mt-10 mb-8 border rounded-lg bg-gray-600 text-white shadow-lg">
         <div class="relative flex gap-4">
-            <img src="https://www.colorado.edu/today/sites/default/files/styles/medium/public/article-image/liu_s-photo.jpg?itok=l-mJPK65" class="relative rounded-lg -top-8 -mb-4 bg-white border h-20 w-20" alt="" loading="lazy" />
+            <img src={userPfp} class="relative rounded-lg -top-8 -mb-4 bg-white border h-20 w-20" alt="" loading="lazy" />
             <div class="flex flex-col w-full">
                 <div class="flex flex-row justify-between">
-                    <p class="relative text-xl whitespace-nowrap truncate overflow-hidden">{props.owner}</p>
+                    <p class="relative text-xl whitespace-nowrap truncate overflow-hidden">{owner.userName}</p>
 
                 </div>
                 <p class="text-sm">{props.date}</p>
