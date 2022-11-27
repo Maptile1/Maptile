f09362fa888339f2f7fad3742fb62bd2392ca766
RobRosloniec <robrosloniec@gmail.com>
2022-11-26 23:14:46 -0500

    Added a textbooks worth of comments.

--- client/src/components/map/EditMap.js
+++ client/src/components/map/EditMap.js
@@ -18,51 +18,24 @@ const EditMap = (props) => {
   const [map, setMap] = useState(null);
   const [gridSize, setGridSize] = useState({ x: 20, y: 20 });
   // ! Layer Formating
-  // * Name: Name of the Layer
-  // ? Name todo: Implement Name Change Function
-  // * Data: 1D array of integers, these represent the tile id
-  // ! Data Important : ID's are shifted by 1 as 0 is a blank space, so the true ID: 0 on the tileset is actually 1
-  // * Active: Flag Determining if the layer is to be rendered, NYI
-  // * Custom Prop: Object that contains a type, ex. : boolean, int , and a value for that prop type
-  // ! Custom Prop: This is for use in a game engine so it must be formatted like the data Tiled (that other app for making tilesets) produces 
+    // * Name: Name of the Layer
+    // ? Name todo: Implement Name Change Function
+    // * Data: 1D array of integers, these represent the tile id
+    // ! Data Important : ID's are shifted by 1 as 0 is a blank space, so the true ID: 0 on the tileset is actually 1
+    // * Active: Flag Determining if the layer is to be rendered, NYI
+    // * Custom Prop: Object that contains a type, ex. : boolean, int , and a value for that prop type
+    // ! Custom Prop: This is for use in a game engine so it must be formatted like the data Tiled (that other app for making tilesets) produces 
+  // ! Map Layers Data vs. Layers state in EditMap
+    // * Both data formats are formatted the same, a 1D array of ints representing ID's
+    // * To get the data from one to the other, you simple take layers[index].data = map.layers[index]
+    // * This is done because there is additional layer data we have that is not in the DB version of the data
   const [layers, setLayers] = useState([{ name: "1", data: [], active: true, customProp: { type: "", value: "" }, id: 0 }])
-  const [layerCount, setLayerCount] = useState(0)
   const [currentLayer, setCurrentLayer] = useState(0)
   const [tileSelection, setTileSelection] = useState([0, 0])
-  const [grid, setGrid] = useState([]);
   const [isMouseDown, setMouseDown] = useState(false)
   var location = useLocation();
 
-  // useEffect(() => {
-  //   let tempGrid = [];
-  //   let counter = 0;
-  //   for (let row = 0; row < 64; row++) {
-  //     const currentRow = [];
-
-  //     for (let col = 0; col < 64; col++) {
-  //       currentRow.push(
-  //         <EditMapGridCell
-  //           id={counter}
-  //           width={gridSize.x}
-  //           height={gridSize.y}
-  //         />
-  //       );
-  //       counter++;
-  //     }
-  //     tempGrid.push(currentRow);
-  //   }
-  //   setGrid(tempGrid);
-  //   const getMap = async () => {
-  //     setLoading(true);
-  //     await Axios.get(
-  //       "https://maptile1.herokuapp.com/map/get/" + location.state._id
-  //     ).then((response) => {
-  //       setMap(response.data.map);
-  //       setLoading(false);
-  //     });
-  //   };
-  //   getMap();
-  // }, [gridSize]);
+  // * This fetches the map data from the DB when first loading the page.
   useEffect(() => {
     const getMap = async () => {
       await Axios.get(
@@ -80,6 +53,8 @@ const EditMap = (props) => {
   }, [])
 
 
+  // * When the map loads and the state changes, this runs to render the inital data
+  // ? TODO: Not actually tested with incoming data, so far only tested with no data and creating a new map.
   useEffect(() => {
     if(!loading){
       if (map.layers.length === 0) {
@@ -87,6 +62,7 @@ const EditMap = (props) => {
         initMap();
       }
       else{
+        // ! This is that part that idk if it works, wont need it until data exporting is setup
         for(let i = 1;i < map.layers.length;i++){
           addNewLayer()
         }
@@ -98,6 +74,7 @@ const EditMap = (props) => {
     }
   }, [map])
 
+  // * Initalizes an empty map with empty cells, only adds one layer
   const initMap = () => {
     // ! GIGA HARD CODE -- Self Explanatory but replace temp width and height with actual values
     let tempW = map.width
@@ -113,6 +90,8 @@ const EditMap = (props) => {
     initDraw()
   }
 
+  // * Draws the data from the initMap function
+  // ? Has a case for non-0 data, probably not needed, will leave for now so it dont break
   const initDraw = () => {
     let canvas = document.querySelector("canvas")
     let ctx = canvas.getContext("2d")
@@ -142,6 +121,8 @@ const EditMap = (props) => {
               crop_size,
               crop_size
             )
+            // * Draws a border rect for illusion of a grid
+            // ? this is not reflected in the data, purley visual
             ctx.globalAlpha = 1
             ctx.strokeStyle = '#000000';  // some color/style
             ctx.lineWidth = 1;
@@ -162,6 +143,8 @@ const EditMap = (props) => {
               crop_size,
               crop_size
             )
+            // * Draws a border rect for illusion of a grid
+            // ? this is not reflected in the data, purley visual
             ctx.strokeStyle = '#000000';  // some color/style
             ctx.lineWidth = 1;
             ctx.strokeRect(x * 16, y * 16, crop_size, crop_size)
@@ -172,7 +155,9 @@ const EditMap = (props) => {
     }
   }
 
+  // * Main drawing method, rerenders whole screen.
   const draw = () => {
+    // * Get the required elements and store them for easy access
     let canvas = document.querySelector("canvas")
     let ctx = canvas.getContext("2d")
     let tilesetImage = document.querySelector("#tileset-source");
@@ -180,8 +165,10 @@ const EditMap = (props) => {
     // ! GIGA HARD CODE -- Replace 16 with Tile Size
     let crop_size = 16
 
+    // * Draw each layer
     layers.forEach((layer) => {
       layer.data.forEach((tile, i) => {
+        // * Layer.active represents the visibility of the layer
         if(layer.active === true){
           // ! GIGA HARD CODE -- Replace 64 with Map Width and Height
           let x = i % 64
@@ -189,6 +176,8 @@ const EditMap = (props) => {
           // ! GIGA HARD CODE -- Replace 64/16 with TilesetWidth/TileWidth or Height, Replace 16 with Tile Size
           let tileX = tile % (64 / 16)
           let tileY = Math.floor((tile - tileX) / (64 / 16))
+          // * 0 case, draws and empty tile
+          // ? The empty tile is chosen in a weird way, may break in the future
           if (tile === 0) {
             ctx.globalAlpha = 0
             ctx.drawImage(
@@ -203,13 +192,15 @@ const EditMap = (props) => {
               crop_size
             )
             ctx.globalAlpha = 1
+            // * Draws a border rect for illusion of a grid
+            // ? this is not reflected in the data, purley visual
             ctx.strokeStyle = '#000000';  // some color/style
             ctx.lineWidth = 1;
             ctx.opacity = 0.5;
             ctx.strokeRect(x * 16, y * 16, crop_size, crop_size)
           }
+          // * Non-zero case
           else {
-
             tileX = (tile - 1) % (64 / 16)
             tileY = Math.floor(((tile - 1) - tileX) / (64 / 16))
             ctx.drawImage(
@@ -223,6 +214,8 @@ const EditMap = (props) => {
               crop_size,
               crop_size
             )
+            // * Draws a border rect for illusion of a grid
+            // ? this is not reflected in the data, purley visual
             ctx.strokeStyle = '#000000';  // some color/style
             ctx.lineWidth = 1;
             ctx.strokeRect(x * 16, y * 16, crop_size, crop_size)
@@ -233,10 +226,12 @@ const EditMap = (props) => {
     })
   }
 
+  // * Addes a tile to the specified mouse location and redraws the canvas
   const placeTile = (e) => {
     let clicked = getCoords(e)
     // ! GIGA HARD CODE -- Replace 64 with Map Height
     let id = clicked[0] + clicked[1] * 64
+    // * Erases data if shift is held and you click
     if (e.shiftKey) {
       layers[currentLayer].data[id] = 0
     }
@@ -247,6 +242,7 @@ const EditMap = (props) => {
     draw()
   }
 
+  // * Returns the grid coords from mouse click location
   function getCoords(e) {
     const { x, y } = e.target.getBoundingClientRect();
     const mouseX = e.clientX - x;
@@ -255,6 +251,8 @@ const EditMap = (props) => {
     return [Math.floor(mouseX / 16), Math.floor(mouseY / 16)];
   }
 
+  // * Handles updating the little blue square that shows which tile is selected
+  // ? Color is picked in 'style.css' because tailwind is being weird, this is later on in render tho
   const tilesetClick = (e) => {
     let tilesetSelection = document.querySelector(".tile-selector");
     let newCoords = getCoords(e)
@@ -267,16 +265,17 @@ const EditMap = (props) => {
     }
   }
 
-
-  const zoom = (factor) => {
-    if (gridSize.x + factor * 10 <= 60 && gridSize.x + factor * 10 >= 20) {
-      setGridSize({ x: gridSize.x + factor * 10, y: gridSize.y + factor * 10 });
-    }
-  };
+  // ! Deprecated, for now, zoom not figured out
+  // const zoom = (factor) => {
+  //   if (gridSize.x + factor * 10 <= 60 && gridSize.x + factor * 10 >= 20) {
+  //     setGridSize({ x: gridSize.x + factor * 10, y: gridSize.y + factor * 10 });
+  //   }
+  // };
   const updateMap = (map) => {
     setMap(map);
   };
 
+  // ? No idea if this actually works, Lin was working on this.
   const handleShare = async (email) => {
     await Axios.post("https://maptile1.herokuapp.com/user/share", {
       type: "map",
@@ -291,6 +290,7 @@ const EditMap = (props) => {
       });
   };
 
+  // * Adds a new layer to the map when the button is pressed
   const addNewLayer = () => {
     let newLayer = { name: layers.length + 1, data: [], active: true, customProp: { type: "", value: "" }, id: layers.length }
     for (let i = 0; i < 64 * 64; i++) {
@@ -299,6 +299,7 @@ const EditMap = (props) => {
     setLayers([...layers, newLayer])
   }
 
+  // * Updated the visibility of a layer and redraws the screen without that layer
   const layerVis = (id) => {
     layers[id].active = !layers[id].active
     draw()
@@ -334,13 +335,13 @@ const EditMap = (props) => {
               <div className="col-start-8 justify-items-start flex flex-row">
                 <button
                   className="text-4xl text-maptile-green cursor-pointer"
-                  onClick={() => zoom(-1)}
+                  
                 >
                   -
                 </button>
                 <button
                   className="ml-5 mr-[-40px] text-4xl text-maptile-green cursor-pointer"
-                  onClick={() => zoom(1)}
+                  
                 >
                   +
                 </button>
@@ -391,6 +392,15 @@ const EditMap = (props) => {
                       <EditMapTileDisplay tile={tile} />
                     ))} */}
                     <div className={`absolute tile-selector left-0 top-0 w-[16px] h-[16px] p-2 z-30`}></div>
+                    {/* This is where the tileset Image is hard coded
+                        you just need to fetch the image from the image db based on the ID of the tileset.
+                        Positioning of the little blue selector square might be weird, so having pages might be the easiest option.
+                        If you do that, you need to mind the id's and keep track of the dimensions of the other tilesets.
+                        A grid of 4x4 TILES(a tile could be 16x16 or whatever, I mean actual 4x4 whole tiles) had 16 unique tiles,
+                        so a second tileset would need to start at the ID of 16 and go to whatever its dimensions define.
+                        NOTE: Tile data is off set by 1 to account for blank spaces. This means tile ID 0 is ID 1 in the data, because 0 is an empty space.
+                        I am 99% sure this is how it should be, this slight difference is already accounted for in the code for getting the tile.
+                        */}
                     <img id="tileset-source" className="" style={{ "imageRendering": "pixelated" }} src={"https://maptilefiles.blob.core.windows.net/maptile-tileset-image/637287cbdf17e9e9316f1cf6"} crossOrigin="true" alt="" />
                   </div>
                 </div>
