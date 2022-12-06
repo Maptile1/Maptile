e64cf840eab162e0b8d188b37664fb201d33952f
Lin Ni <78550234+linni123789@users.noreply.github.com>
2022-12-06 09:57:59 -0500

    fixed unknown image for map

--- client/src/components/card/MapCard.js
+++ client/src/components/card/MapCard.js
@@ -1,32 +1,44 @@
 import { useNavigate } from "react-router-dom";
-import { React, useEffect, useState} from 'react'
+import { React, useEffect, useState } from "react";
 const MapCard = (props) => {
   const nav = useNavigate();
-  const [image, setImage] = useState("https://maptilefiles.blob.core.windows.net/maptile-map-image/" + props._id + "?=" + Math.random().toString().substring(2))
+  const [image, setImage] = useState(
+    "https://maptilefiles.blob.core.windows.net/maptile-map-image/" +
+      props._id +
+      "?=" +
+      Math.random().toString().substring(2)
+  );
 
   const handleMapView = () => {
-    nav("/maps/" + props._id, { state: { owner: props.owner, _id: props._id } });
+    nav("/maps/" + props._id, {
+      state: { owner: props.owner, _id: props._id },
+    });
   };
 
   useEffect(() => {
-    setImage("https://maptilefiles.blob.core.windows.net/maptile-map-image/" + props._id + "?=" + Math.random().toString().substring(2)) 
-  }, [props._id])
+    setImage(
+      "https://maptilefiles.blob.core.windows.net/maptile-map-image/" +
+        props._id +
+        "?=" +
+        Math.random().toString().substring(2)
+    );
+  }, [props._id]);
 
   return (
     <div class="max-w-sm rounded overflow-hidden shadow-lg border bg-maptile-background-mid">
       <img
-          class="w-full h-3/4 border border-white cursor-pointer object-cover object-center"
-          style={{"image-rendering" : "pixelated"}}
-          src={image}
-          alt=""
-          onError={({ currentTarget }) => {
-            currentTarget.onerror = null;
-            setImage(
-              "https://maptilefiles.blob.core.windows.net/maptile-map-image/6372801adf17e9e9316f1b4c"
-            );
-          }}
-          onClick={handleMapView}
-        />
+        class="w-full h-3/4 border border-white cursor-pointer object-cover object-center"
+        style={{ "image-rendering": "pixelated" }}
+        src={image}
+        alt=""
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null;
+          setImage(
+            "https://maptilefiles.blob.core.windows.net/maptile-tileset-image/6372801adf17e9e9316f1b4c"
+          );
+        }}
+        onClick={handleMapView}
+      />
       <div class="px-6 py-4 cursor-pointer" onClick={handleMapView}>
         <div class="font-bold text-white text-xl mb-2">{props.name}</div>
         <p class="text-white text-base">{props.description}</p>
